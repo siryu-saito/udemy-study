de6d5c6355439965f88cccc3d97d1e2c65703688
siryu-saito <saito.s.ryu@gmail.com>
2021-10-12 13:28:30 +0900

    JavaScript復習

---
+++ QUIZ/questions.js
@@ -0,0 +1,42 @@
+const answersList = document.querySelectorAll('ol.answers li');
+
+answersList.forEach(li => li.addEventListener('click', checkClickedAnswer));
+
+// 正しい答え
+const correctAnswers = {
+  question1: 'B',
+  question2: 'A',
+};
+
+function checkClickedAnswer(event) {
+  const checkedAnswerElement =  event.currentTarget;
+  // 選択した答え
+  const selectedAnswer = checkedAnswerElement.dataset.answer;
+  const questionId= checkedAnswerElement.closest('ol.answers').dataset.id;
+  // 正しい答え
+  const correctAnswer = correctAnswers[questionId];
+  // メッセージを入れる変数を用意
+  let message;
+  // カラーコードを入れる変数を用意
+  let answerColorCode;
+
+  if (selectedAnswer == correctAnswer) {
+    // 正解時
+    message = '正解です！';
+    answerColorCode = '';
+  } else {
+    // 不正解時
+    message = '残念、不正解です';
+    answerColorCode = 'red';
+  }
+
+
+  alert(message);
+
+  // // 色を変更(不正解時)
+  // document.querySelector('#section-correct-answer p').style.color = 'red';
+
+  // // 答えを表示
+  document.querySelector('#section-correct-answer').style.display = 'block';
+}
+
